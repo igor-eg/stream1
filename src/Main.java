78abc5f06d45f9a0ca4f8673025d8568a197fea0
Igor Egorov <igor73@list.ru>
2021-12-21 15:51:34 +0300

    Первоначальная версия

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        Collections.sort(intList);
+        int count = 0;
+        for (int x : intList) {
+            if (x > 0 && (x % 2 == 0)) {
+
+                System.out.println(x);
+            }
+        }
+
+        System.out.println("Hello world!");
+    }
+}
